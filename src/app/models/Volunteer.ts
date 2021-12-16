e21d56877ef6703935b51acc0e6c77383191fbc9
jwhammer59 <jeff@jhammermedia>
2021-12-16 09:41:15 -0500

    add volunteer class stop tracking environment files

---
+++ src/app/models/Volunteer.ts
@@ -0,0 +1,12 @@
+export class Volunteer {
+  id?: string;
+  firstName: string = '';
+  lastName: string = '';
+  familyID: string = '';
+  address1: string = '';
+  address2: string = '';
+  city: string = '';
+  state: string = '';
+  zipcode: number = 0;
+  isAvailable: boolean = false;
+}
